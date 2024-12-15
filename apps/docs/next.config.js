74262f36422ad176aff2419b817f053d01e0122f
binsan song <qlstks@gmail.com>
2024-12-15 20:44:29 +0900

    docs 환경변수 없으면 에러발생하는 코드 추가

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+assertEnv();
+
 const nextConfig = {};
 
 export default nextConfig;
+
+/** 환경변수 검증 */
+function assertEnv() {
+  const variables = [
+    "EXAMPLE_DB_URL",
+  ];
+
+  const missingVariables = variables.filter((v) => !process.env[v]);
+
+  if (missingVariables.length > 0) {
+    throw new Error(`Missing environment variables: ${missingVariables.join(", ")}`);
+  }
+}
